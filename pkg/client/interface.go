0a8b552d6cb6ac5391c29a6b0e33be9a13c327fe
ecclesiafoundation <143023386+ecclesiafoundation@users.noreply.github.com>
2023-08-24 16:52:09 -0400

    add comments for Prettify function and move to top of interface.go

--- pkg/client/interface.go
+++ pkg/client/interface.go
@@ -5,6 +5,11 @@ import (
 	"ecclesiafoundation.org/APIBibleClient/pkg/client/params"
 )
 
+// Prettify pretty-prints the JSON response
+func Prettify(json string) (string, error) {
+	return internal.PrettifyJson(json)
+}
+
 // RetrieveApiKey retrieves API key from
 // the environment variable, SCRIPTURE_API_BIBLE_KEY
 func RetrieveApiKey() (string, error) {
@@ -14,8 +19,4 @@ func RetrieveApiKey() (string, error) {
 // The RetrieveBibles function calls the `bibles` end point of the API
 func RetrieveBibles(apiKey string, params params.BiblesParams) (string, error) {
 	return internal.RetrieveBibles(apiKey, params)
-}
-
-func Prettify(json string) (string, error) {
-	return internal.PrettifyJson(json)
 }
