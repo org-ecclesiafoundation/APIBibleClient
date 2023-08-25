ba643fd64974c7bf26d7c2487d116445cd1c7433
brain-fuel <matt@brain-fuel.co>
2023-08-24 21:10:55 -0400

    update documentation

--- pkg/client/interface.go
+++ pkg/client/interface.go
@@ -11,11 +11,12 @@ func RetrieveApiKey() (string, error) {
 	return internal.RetrieveApiKey()
 }
 
-// The RetrieveBibles function calls the `bibles` end point of the API
+// The RetrieveBibles function calls the `/bibles` end point of the API
 func RetrieveBibles(apiKey string, params params.BiblesParams) (string, error) {
 	return internal.RetrieveBibles(apiKey, &params)
 }
 
+// The RetrieveBibleById function calls the `/bibles/{bibleId}` end point of the API
 func RetrieveBibleById(apiKey string, bibleId string) (string, error) {
 	return internal.RetrieveBibleById(apiKey, bibleId)
 }
