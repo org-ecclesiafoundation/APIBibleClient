9993956d728aa7e2b224849f2131d54c9c08f0e4
brain-fuel <matt@brain-fuel.co>
2023-09-04 20:56:36 -0400

    add example of GetBibleById to test/doc

--- pkg/client/interface_test.go
+++ pkg/client/interface_test.go
@@ -34,6 +34,7 @@ func ExampleGetBibles() {
 			IncludeFullDetails: true,
 		}
 		bibles, biblesErr := GetBibles(apiKey, &biblesParams)
+		// Here is some boilerplate for handling errors and pretty-printing
 		if biblesErr != nil {
 			fmt.Println("Do error handling for failing to get bibles here")
 		} else {
@@ -80,5 +81,64 @@ func ExampleGetBibles() {
 	//       "updatedAt": "2023-05-03T21:21:08.000Z"
 	//     }
 	//   ]
+	// }
+}
+
+func ExampleGetBibleById() {
+	apiKey, apiKeyErr := utils.GetApiKey()
+	kjvBibleId := "de4e12af7f28f599-02"
+	var output string
+	if apiKeyErr != nil {
+		fmt.Printf("Failed to get API key.\n"+
+			"Please set the environment variable %s to the appropriate value",
+			API_KEY_ENV_VAR)
+	} else {
+		// Here is an example of an API call
+		bible, bibleErr := GetBibleById(apiKey, kjvBibleId)
+		// Here is some boilerplate for handling errors and pretty-printing
+		if bibleErr != nil {
+			fmt.Println("Do error handling for failing to get bibles here")
+		} else {
+			prettyBible, prettifyErr := utils.Prettify(bible)
+			if prettifyErr != nil {
+				fmt.Println("Do error handling for failing to print bibles JSON here")
+			} else {
+				output = prettyBible
+			}
+		}
+	}
+	fmt.Println(output)
+	// Output:
+	// {
+	//   "data": {
+	//     "abbreviation": "engKJV",
+	//     "abbreviationLocal": "KJV",
+	//     "audioBibles": [],
+	//     "copyright": "PUBLIC DOMAIN except in the United Kingdom, where a Crown Copyright applies to printing the KJV. See http://www.cambridge.org/about-us/who-we-are/queens-printers-patent",
+	//     "countries": [
+	//       {
+	//         "id": "GB",
+	//         "name": "United Kingdom of Great Britain and Northern Ireland",
+	//         "nameLocal": "United Kingdom of Great Britain and Northern Ireland"
+	//       }
+	//     ],
+	//     "dblId": "de4e12af7f28f599",
+	//     "description": "Protestant",
+	//     "descriptionLocal": "Protestant",
+	//     "id": "de4e12af7f28f599-02",
+	//     "info": "\u003cp\u003eThis historical translation of the Holy Bible is brought to you in quality digital format by the \u003ca href=\"https://crosswire.org/\"\u003eCrosswire Bible Society\u003c/a\u003e and \u003ca href=\"https://eBible.org\"\u003eeBible.org\u003c/a\u003e.\u003c/p\u003e",
+	//     "language": {
+	//       "id": "eng",
+	//       "name": "English",
+	//       "nameLocal": "English",
+	//       "script": "Latin",
+	//       "scriptDirection": "LTR"
+	//     },
+	//     "name": "King James (Authorised) Version",
+	//     "nameLocal": "King James Version",
+	//     "relatedDbl": null,
+	//     "type": "text",
+	//     "updatedAt": "2023-05-03T09:23:59.000Z"
+	//   }
 	// }
 }
